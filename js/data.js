b4a6af822d27a72d528813e7dab09eb86a9543fc
程序员·小李 <875402161@qq.com>
2023-09-30 06:46:53 +0000

    update js/data.js.
    Signed-off-by: 程序员·小李 <875402161@qq.com>

--- js/data.js
+++ js/data.js
@@ -1,3 +1,5 @@
+import {learning} from 'learningData'
+
 // 导航列表
 let guideTabListData = [
     {
@@ -26,91 +28,91 @@ let guideTabListData = [
 let guideTabGroupListData = [];
 
 
-let learning = [
-	{
-		num: 1,
-		data:[
-			{
-				src: 'https://blog.csdn.net/Day_and_Night_2017',
-				alt: '我在csdn'
-			},
-			{
-				src: 'https://www.cnblogs.com',
-				alt: 'cnblogs'
-			},
-			{
-				src: 'https://www.geeksforgeeks.org',
-				alt: '极客算法'
-			},
-			{
-				src: 'https://mvnrepository.com',
-				alt: 'maven库'
-			},
-			{
-				src: 'https://space.bilibili.com/14163840/channel/seriesdetail?sid=433088',
-				alt: '黎曼猜想'
-			},
-            {
-                src: 'https://hipstersound.com/ambience.html',
-                alt: '白噪声'
-            },
-            {
-                src: 'https://tool.lu/timestamp',
-                alt: '在线时间戳转换'
-            },
-            {
-                src: 'https://www.processon.com',
-                alt: '在线流程图'
-            },{
-                src: 'https://books.halfrost.com/leetcode/ChapterTwo/Array',
-                alt: 'Leetcode解析'
-            },{
-                src: 'https://doocs.gitee.io/source-code-hunter',
-                alt: '源码系列'
-            },{
-                src: 'https://doocs.gitee.io/leetcode',
-                alt: '算法题系列'
-            },{
-                src: 'https://hellokoding.com',
-                alt: 'HelloKoding'
-            },{
-                src: 'https://doocs.gitee.io/advanced-java',
-                alt: '面试扫盲'
-            },{
-                src: 'https://labuladong.gitee.io/algo',
-                alt: 'labuladong刷题'
-            },{
-                src: 'https://doocs.github.io/jvm/00-quickstart.html',
-                alt: 'JVM解析'
-            },{
-                src: 'https://javanote.doc.lewky.cn/#/README',
-                alt: 'JAVA Note'
-            },{
-                src: 'http://www.cyc2018.xyz',
-                alt: 'JAVA八股文'
-            },{
-                src: 'https://crossoverjie.top/JCSprout',
-                alt: 'JCSprout'
-            },{
-                src: 'https://tobebetterjavaer.com/home.html',
-                alt: 'JAVA进阶'
-            },{
-                src: 'https://javaguide.cn/home.html',
-                alt: 'JAVA Guide'
-            },{
-                src: 'https://pdai.tech',
-                alt: 'JAVA 全栈'
-            },{
-                src: 'https://juejin.cn/backend',
-                alt: '掘金'
-            },{
-                src: 'https://dayAndnight2018.github.io/career-exam',
-                alt: '事业单位'
-            }
+// let learning = [
+// 	{
+// 		num: 1,
+// 		data:[
+// 			{
+// 				src: 'https://blog.csdn.net/Day_and_Night_2017',
+// 				alt: '我在csdn'
+// 			},
+// 			{
+// 				src: 'https://www.cnblogs.com',
+// 				alt: 'cnblogs'
+// 			},
+// 			{
+// 				src: 'https://www.geeksforgeeks.org',
+// 				alt: '极客算法'
+// 			},
+// 			{
+// 				src: 'https://mvnrepository.com',
+// 				alt: 'maven库'
+// 			},
+// 			{
+// 				src: 'https://space.bilibili.com/14163840/channel/seriesdetail?sid=433088',
+// 				alt: '黎曼猜想'
+// 			},
+//             {
+//                 src: 'https://hipstersound.com/ambience.html',
+//                 alt: '白噪声'
+//             },
+//             {
+//                 src: 'https://tool.lu/timestamp',
+//                 alt: '在线时间戳转换'
+//             },
+//             {
+//                 src: 'https://www.processon.com',
+//                 alt: '在线流程图'
+//             },{
+//                 src: 'https://books.halfrost.com/leetcode/ChapterTwo/Array',
+//                 alt: 'Leetcode解析'
+//             },{
+//                 src: 'https://doocs.gitee.io/source-code-hunter',
+//                 alt: '源码系列'
+//             },{
+//                 src: 'https://doocs.gitee.io/leetcode',
+//                 alt: '算法题系列'
+//             },{
+//                 src: 'https://hellokoding.com',
+//                 alt: 'HelloKoding'
+//             },{
+//                 src: 'https://doocs.gitee.io/advanced-java',
+//                 alt: '面试扫盲'
+//             },{
+//                 src: 'https://labuladong.gitee.io/algo',
+//                 alt: 'labuladong刷题'
+//             },{
+//                 src: 'https://doocs.github.io/jvm/00-quickstart.html',
+//                 alt: 'JVM解析'
+//             },{
+//                 src: 'https://javanote.doc.lewky.cn/#/README',
+//                 alt: 'JAVA Note'
+//             },{
+//                 src: 'http://www.cyc2018.xyz',
+//                 alt: 'JAVA八股文'
+//             },{
+//                 src: 'https://crossoverjie.top/JCSprout',
+//                 alt: 'JCSprout'
+//             },{
+//                 src: 'https://tobebetterjavaer.com/home.html',
+//                 alt: 'JAVA进阶'
+//             },{
+//                 src: 'https://javaguide.cn/home.html',
+//                 alt: 'JAVA Guide'
+//             },{
+//                 src: 'https://pdai.tech',
+//                 alt: 'JAVA 全栈'
+//             },{
+//                 src: 'https://juejin.cn/backend',
+//                 alt: '掘金'
+//             },{
+//                 src: 'https://dayAndnight2018.github.io/career-exam',
+//                 alt: '事业单位'
+//             }
             
-		]
-	}
-]
+// 		]
+// 	}
+// ]
 
 let movies = [
 	{
