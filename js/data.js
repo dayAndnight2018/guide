b8055139bdc29258611f2fb307c55aa82a7752bf
程序员·小李 <875402161@qq.com>
2023-10-08 08:33:33 +0000

    update js/data.js.
    Signed-off-by: 程序员·小李 <875402161@qq.com>

--- js/data.js
+++ js/data.js
@@ -2,6 +2,10 @@
 let guideTabListData = [
     {
         title: '编程',
+        num: 0,
+    },
+    {
+        title: '进阶',
         num: 1,
     },
     {
@@ -25,10 +29,9 @@ let guideTabListData = [
 
 let guideTabGroupListData = [];
 
-
-let learning = [
-	{
-		num: 1,
+let program = [
+   {
+		num: 0,
 		data:[
 			{
 				src: 'https://blog.csdn.net/Day_and_Night_2017',
@@ -39,29 +42,31 @@ let learning = [
 				alt: 'cnblogs'
 			},
 			{
-				src: 'https://www.geeksforgeeks.org',
-				alt: '极客算法'
-			},
-			{
 				src: 'https://mvnrepository.com',
 				alt: 'maven库'
+			},
+            {
+                src: 'https://juejin.cn/backend',
+                alt: '掘金'
+            }
+            
+		]
+	} 
+]
+
+let learning = [
+	{
+		num: 1,
+		data:[
+			{
+				src: 'https://www.geeksforgeeks.org',
+				alt: '极客算法'
 			},
 			{
 				src: 'https://space.bilibili.com/14163840/channel/seriesdetail?sid=433088',
 				alt: '黎曼猜想'
 			},
             {
-                src: 'https://hipstersound.com/ambience.html',
-                alt: '白噪声'
-            },
-            {
-                src: 'https://tool.lu/timestamp',
-                alt: '在线时间戳转换'
-            },
-            {
-                src: 'https://www.processon.com',
-                alt: '在线流程图'
-            },{
                 src: 'https://books.halfrost.com/leetcode/ChapterTwo/Array',
                 alt: 'Leetcode解析'
             },{
@@ -97,9 +102,6 @@ let learning = [
             },{
                 src: 'https://pdai.tech',
                 alt: 'JAVA 全栈'
-            },{
-                src: 'https://juejin.cn/backend',
-                alt: '掘金'
             },{
                 src: 'https://dayAndnight2018.github.io/career-exam',
                 alt: '事业单位'
@@ -356,7 +358,17 @@ let tools = [
             },{
                 src: 'https://www.bear20.com/',
                 alt: '小熊下载站'
-            }
+            },{
+                src: 'https://hipstersound.com/ambience.html',
+                alt: '白噪声'
+            },{
+                src: 'https://tool.lu/timestamp',
+                alt: '在线时间戳转换'
+            },
+            {
+                src: 'https://www.processon.com',
+                alt: '在线流程图'
+            },
 		]
 	}
 ]
@@ -377,6 +389,7 @@ let amusement = [
 	}
 ]
 
+guideTabGroupListData = guideTabGroupListData.concat(program);
 guideTabGroupListData = guideTabGroupListData.concat(learning);
 guideTabGroupListData = guideTabGroupListData.concat(movies);
 guideTabGroupListData = guideTabGroupListData.concat(books);
